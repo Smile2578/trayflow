c9053d2220f96dd881ac62ac82af43062e55c862
Smile2578 <107757684+Smile2578@users.noreply.github.com>
2023-09-07 14:14:16 +0200

    Update uploadToGCS.js

--- pages/api/uploads/uploadToGCS.js
+++ pages/api/uploads/uploadToGCS.js
@@ -1,7 +1,6 @@
 import multer from 'multer';
-import { initGoogleCloudStorage, getGCSBucket } from '../../../utils/gcs';
+import { initGoogleCloudStorage, getGCSBucket, generateSignedUrl } from '../../../utils/gcs';
 
-// Disable global body parser for this route
 export const config = {
   api: {
     bodyParser: false,
@@ -11,56 +10,51 @@ export const config = {
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 55 * 1024 * 1024 }, // 55 MB
+  limits: { fileSize: 100 * 1024 * 1024 },
 });
 
 export default async function handler(req, res) {
-  // Initial log
-  console.log("Handler called with method:", req.method);
+  await initGoogleCloudStorage();
 
-  await initGoogleCloudStorage();  // Initialize GCS
-  const bucket = getGCSBucket();  // Get the initialized bucket
-
-  if (req.method !== 'POST') {
-    console.log("Invalid method attempted");
-    return res.status(405).json({ error: 'Method not allowed' });
+  if (req.method === 'GET') {
+    const filename = req.query.filename;
+    if (!filename) {
+      return res.status(400).json({ error: 'Filename is required to generate a signed URL.' });
+    }
+    try {
+      const signedUrl = await generateSignedUrl(filename);
+      return res.status(200).json({ url: signedUrl });
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to generate signed URL.' });
+    }
   }
 
   upload.single('file')(req, res, async (err) => {
     if (err instanceof multer.MulterError) {
-      console.error("Multer error:", err);
       return res.status(400).json({ error: err.message });
     } else if (err) {
-      console.error("Unknown error:", err);
       return res.status(500).json({ error: err.message });
     }
 
     if (!req.file || !req.file.buffer || !req.file.originalname) {
-      console.error("File missing in request");
       return res.status(400).json({ error: 'No file attached in the request' });
     }
 
     try {
-      const blob = bucket.file(req.file.originalname);
+      const blob = getGCSBucket().file(req.file.originalname);
       const blobStream = blob.createWriteStream();
 
       blobStream.on('error', (blobErr) => {
-        console.error("Blob stream error:", blobErr);
         res.status(500).json({ error: blobErr.message });
-        return;
       });
 
       blobStream.on('finish', () => {
-        console.log("Blob stream finished");
         res.status(200).json({ filename: req.file.originalname });
-        return;
       });
 
       blobStream.end(req.file.buffer);
     } catch (storageErr) {
-      console.error("Storage error:", storageErr);
       res.status(500).json({ error: storageErr.message });
-      return;
     }
   });
 }
