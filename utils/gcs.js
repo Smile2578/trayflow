89454406d807ff136a177e64280b32272842da37
Smile2578 <107757684+Smile2578@users.noreply.github.com>
2023-09-09 12:42:21 +0200

    test

--- utils/gcs.js
+++ utils/gcs.js
@@ -5,41 +5,42 @@ const client = new SecretManagerServiceClient();
 let storage;
 let bucket;
 
-// Use the provided environment variable names
-const { GCP_SERVICE_ACCOUNT, GOOGLE_APPLICATION_CREDENTIALS, BUCKET_NAME } = process.env;
+// Use the correct environment variable name
+const { BUCKET_NAME } = process.env;
 
-async function getSecret() {
-  // Use GCP_SERVICE_ACCOUNT for accessing the secret
-  const [version] = await client.accessSecretVersion({
-      name: GCP_SERVICE_ACCOUNT,
-  });
-
-  const secretData = version.payload.data.toString('utf8');
-  return JSON.parse(secretData);
+async function getSecret(secretName) {
+  try {
+    const [version] = await client.accessSecretVersion({ name: secretName });
+    const secretData = version.payload.data.toString('utf8');
+    return JSON.parse(secretData);
+  } catch (error) {
+    console.error(`Failed to retrieve secret ${secretName}:`, error.message);
+    throw error;
+  }
 }
 
 export async function initGoogleCloudStorage() {
   try {
-      console.log("Initializing Google Cloud Storage with bucket:", BUCKET_NAME);
+    console.log("Initializing Google Cloud Storage with bucket:", BUCKET_NAME);
 
-      // Use GOOGLE_APPLICATION_CREDENTIALS to get the secret
-      const googleCloudConfig = await getSecret();
+    const secretName = `projects/${process.env.GOOGLE_PROJECTID}/secrets/trayflow_service_account/versions/latest`;
+    const googleCloudConfig = await getSecret(secretName);
 
-      storage = new Storage({
-          credentials: googleCloudConfig
-      });
+    storage = new Storage({
+      credentials: googleCloudConfig
+    });
 
-      bucket = storage.bucket(BUCKET_NAME);
-      console.log("Google Cloud Storage initialized.");
+    bucket = storage.bucket(BUCKET_NAME);
+    console.log("Google Cloud Storage initialized.");
   } catch (error) {
-      console.error("Failed to initialize Google Cloud Storage:", error.message);
-      throw error;
+    console.error("Failed to initialize Google Cloud Storage:", error.message);
+    throw error;
   }
 }
 
 export function getGCSBucket() {
   if (!bucket) {
-      throw new Error("Google Cloud Storage is not initialized.");
+    throw new Error("Google Cloud Storage is not initialized.");
   }
   return bucket;
 }
