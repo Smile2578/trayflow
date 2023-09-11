42c1ae3e9b2d3c45c908b70d83ecbed726041ce0
Smile2578 <107757684+Smile2578@users.noreply.github.com>
2023-09-11 11:30:24 +0200

    post gcs

--- pages/api/uploads/uploadToGCS.js
+++ pages/api/uploads/uploadToGCS.js
@@ -2,6 +2,10 @@ import { getSession } from 'next-auth/react';
 import { generateV4UploadSignedUrl } from '../../../utils/gcs';
 
 export default async function handler(request, response) {
+    if (request.method !== 'POST') {
+        return response.status(405).end();  // Only allow POST method
+    }
+
     try {
         // 1. Authentication
         const session = await getSession({ req: request });
@@ -9,19 +13,24 @@ export default async function handler(request, response) {
             return response.status(401).json({ error: 'Not authenticated' });
         }
 
-        // 2. Authorization (Si on veut rajouter une selection sur le rôle)
+        // 2. Authorization (If you want to add role-based checks later)
         // if (session.user.role !== "allowedRole") {
         //     return response.status(403).json({ error: 'Not authorized' });
         // }
 
         // 3. Validation
         const { fileName, contentType } = request.body;
-        if (!fileName || !contentType) {
-            return response.status(400).json({ error: 'Invalid input' });
+
+        if (!fileName) {
+            return response.status(400).json({ error: 'File name is required.' });
+        }
+
+        if (!contentType) {
+            return response.status(400).json({ error: 'Content type is required.' });
         }
 
         if (contentType !== 'model/stl') { // Only allowing STL files
-            return response.status(400).json({ error: 'Unsupported file type' });
+            return response.status(400).json({ error: 'Unsupported file type. Only .stl files are allowed.' });
         }
 
         // Generate signed URL for uploading
@@ -31,7 +40,7 @@ export default async function handler(request, response) {
         return response.status(200).json({ signedUrl });
 
     } catch (error) {
-        console.error("Error in uploadToGCS:", error.message);
-        return response.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error in uploadToGCS:", error);
+        return response.status(500).json({ error: 'Internal Server Error: ' + error.message });
     }
 }
